32b9b9689ab6fdfd4a22035c5dc4847dec909bbc
parikhitritgithub <parikhitkurmi44@gmail.com>
2024-07-14 20:13:12 +0530

    bugs fixed

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,10 +6,6 @@ export default function Sidebar() {
     <section className='sticky left-0 top-0 flex h-screen w-fit flex-col justify between  bg-dark-1 p-6  pt-28  text-white max-sm:hidden lg:w-[264px]'>
       
       <div className='flex flex- flex-col gap-6 '>
-        {[
-          {
-            imgUrl 
-          }
-        ]}</div> sidebar </section>
+      </div> sidebar </section>
   )
 }
